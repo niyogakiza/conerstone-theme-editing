e2a0ce10cb29447f3a58a546dae2fd04977cc386
Aimable <niyoai@yahoo.fr>
2018-05-02 11:47:20 +0100

    navigation component modifications

--- assets/js/theme/common/sticky-menu.js
+++ assets/js/theme/common/sticky-menu.js
@@ -1,15 +1,18 @@
-const nav = document.querySelector('.navPages-container');
-const topOfNav = nav.offsetTop;
+export default function stickyMenu() {
+    const nav = document.querySelector('.navPages-container');
+    const topOfNav = nav.offsetTop;
 
-function fixNav() {
-    // console.log(topOfNav, window.screenY);
-    if (window.scrollY >= topOfNav) {
-        document.body.style.paddingTop = `${nav.offsetHeight}px`;
-        document.body.classList.add('fixed-nav');
-    } else {
-        document.body.style.paddingTop = 0;
-        document.body.classList.remove('fixed-nav');
+    function fixNav() {
+        // console.log(topOfNav, window.screenY);
+        if (window.scrollY >= topOfNav) {
+            document.body.style.paddingTop = `${nav.offsetHeight}px`;
+            document.body.classList.add('fixed-nav');
+        } else {
+            document.body.style.paddingTop = 0;
+            document.body.classList.remove('fixed-nav');
+        }
     }
+
+    window.addEventListener('scroll', fixNav);
 }
 
-window.addEventListener('scroll', fixNav);
